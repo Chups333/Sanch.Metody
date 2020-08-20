bb3297cbe17c89c873db14857a01e77c55865a69
Petrov Sasha <sasha1327@mail.ru>
2020-08-20 18:39:19 +0800

    Добавьте файлы проекта.

---
+++ Sanch.Metody/Person.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sanch.Metody
+{
+    public class Person
+    {
+
+        public string SecondName { get; set; }
+
+        public string Name { get; set; }
+
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Person (string secondName, string name)
+        {
+            SecondName = secondName;
+            Name = name;
+            X = 0;
+            Y = 0;
+        }
+
+        public string Run()
+        {
+            var rnd = new Random();
+
+            X += rnd.Next(-2, 2);
+            Y += rnd.Next(-2, 2);
+
+            return $"{Name} ({X}),({Y})";
+        }
+
+        // Перегрузка метода
+
+        /*public string RunToPos(int x,int y) // так делать не надо
+        {
+            X = x;
+            Y = y;
+
+            return $"{Name} ({X}),({Y})";
+        }
+
+        public string Run(int x, int y) // а так надо !!!! перегрузка с аргументами
+        {
+
+            return $"{Name} ({X}),({Y})";
+        }
+
+        public string Run(int x) // перегрузка с одной сигнатурой 
+        {
+
+            return $"{Name} ({X}),({Y})";
+        }
+
+        public string Run(double x) // перегрузка с одной сигнатурой которая не повторяет верхнюю (другой тип сигнатуры)
+        {
+
+            return $"{Name} ({X}),({Y})";
+        }*/
+    }
+}
---
+++ Sanch.Metody/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sanch.Metody
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // PrintHello("Вася", 27); - сработает метод внизу
+            //var i = PrintHello("Вася", 27); - i вернет цифру 2
+
+            //var person1 = new Person("Иванов", "Иван");
+            //var person2 = new Person("Петров", "Петр");
+
+            /*for (var i = 0; i < 10; i++)
+            {
+                var pos = person1.Run();
+                Console.WriteLine(pos);
+
+                Console.WriteLine(person2.Run());
+            }*/
+
+            Console.WriteLine(Factorial(3)); // вывод рекурсии ниже
+            Console.ReadKey();
+        }
+
+        // модификатор доступа тип возвращаемого значения имя метода(тип аркумента и имя(мб несколько))
+        //{
+        //}
+        // void - тип невозращаемого значения
+        // класс тоже тип возвращаемого значения 
+        // static экзепляр должен быть общее для всех экземпляров класса 
+
+        public static int PrintHello(string name, int age)
+        {
+            Console.WriteLine($"Привет, {name}, Вам {age} лет ");
+            return 2;// обязательно для возвращаемого значения 
+        }
+
+        /*public static void PrintHello(string name, int age) ничего не возвращает
+        {
+            Console.WriteLine($"Привет, {name}, Вам {age} лет ");
+           
+        }*/
+
+            // РЕКУРСИЯ
+            public static int Factorial(int value)
+        {
+            if (value <= 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return value * Factorial(value - 1);
+            }
+        }
+    }
+}
